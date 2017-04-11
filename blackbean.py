f0b7d1e7fd2b36c7600d34b8f0f6b1fc90739775
adam nunez <adam.a.nunez@gmail.com>
2017-04-10 21:23:13 -0500

    opcode structure complete

--- blackbean.py
+++ blackbean.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 import sys
-import os
 import argparse
 
 # Features:
@@ -24,21 +23,61 @@ import argparse
 #   Tags are at the start of the line
 #   Pre proc commands don't share a line with asm
 
-# Statment:
-#
-#
-#
-#
-#
-#
+# Line Properties:
+#   isEmpty    - bool
+#   original   - whole, unedited line
+#   comment    - comment for the line
+#   tag        - mem tag for the line
+#   preProcess - Whole line, not used right now
+#   dataType   - 1,2,4
+#       declarations - [list of data decl]
+#   asm
 
-PRE_PROC=("ifdef","else","endif","option","align","equ","=")
-DATA_DECLARE={"db":1,"dw":2,"dd":4}
-REGISTERS={ "v0":0x0,"v1":0x1,"v2":0x2,"v3":0x3,
-            "v4":0x4,"v5":0x5,"v6":0x6,"v7":0x7,
-            "v8":0x8,"v9":0x9,"va":0xA,"vb":0xB,
-            "vc":0xC,"vd":0xD,"ve":0xE,"vf":0xF}
-OP_CODES={}
+#Opcode reminders: SHR, SHL, XOR, and SUBN/SM are NOT offically supported by original spec
+#                  SHR and SHL may or may not move Y (Shifted) into X or just shift X.
+
+HEX_ESC = '#'
+PRE_PROC=('ifdef','else','endif','option','align','equ','=')
+DATA_DECLARE={'db':1,'dw':2,'dd':4}
+REGISTERS={ 'v0':0x0,'v1':0x1,'v2':0x2,'v3':0x3,   #Pretty sure we don't need this...
+            'v4':0x4,'v5':0x5,'v6':0x6,'v7':0x7,
+            'v8':0x8,'v9':0x9,'va':0xA,'vb':0xB,
+            'vc':0xC,'vd':0xD,'ve':0xE,'vf':0xF}
+OP_CODES={  'cls' :[{'args':0,'machine':'00E0'}],
+            'ret' :[{'args':0,'machine':'00EE'}],
+            'sys' :[{'args':1,'machine':'1xxx',1:'address'}],
+            'call':[{'args':1,'machine':'1xxx',1:'address'}],
+            'skp' :[{'args':1,'machine':'Ex9E',1:'register'}],
+            'sknp':[{'args':1,'machine':'ExA1',1:'register'}],
+            'se'  :[{'args':2,'machine':'5xy0',1:'register',2:'register'},
+		            {'args':2,'machine':'3xyy',1:'register',2:'byte'}],
+            'sne' :[{'args':2,'machine':'9xy0',1:'register',2:'register'},
+		            {'args':2,'machine':'4xyy',1:'register',2:'byte'}],
+            'add' :[{'args':2,'machine':'7xyy',1:'register',2:'byte'},
+		            {'args':2,'machine':'8xy4',1:'register',2:'register'},
+		            {'args':2,'machine':'Fx1E',1:'i',2:'register'}],
+            'or'  :[{'args':2,'machine':'8xy1',1:'register',2:'register'}],
+            'and' :[{'args':2,'machine':'8xy2',1:'register',2:'register'}],
+            'xor' :[{'args':2,'machine':'8xy3',1:'register',2:'register'}],
+            'sub' :[{'args':2,'machine':'8xy5',1:'register',2:'register'}],
+            'subn':[{'args':2,'machine':'8xy7',1:'register',2:'register'}],
+            'shr' :[{'args':2,'machine':'8xy6',1:'register',2:'register'}],
+            'shl' :[{'args':2,'machine':'8xyE',1:'register',2:'register'}],
+            'rnd' :[{'args':2,'machine':'cxyy',1:'register',2:'byte'}],
+            'jp'  :[{'args':1,'machine':'1xxx',1:'address'}},
+		            {'args':2,'machine':'Byyy',1:'v0',2:'address'}}],
+            'ld'  :[{'args':2,'machine':'6xyy',1:'register',2:'byte'},
+		            {'args':2,'machine':'8xy0',1:'register',2:'register'},
+		            {'args':2,'machine':'Fx07',1:'register',2:'dt'},
+		            {'args':2,'machine':'Fx0A',1:'register',2:'k'},
+		            {'args':2,'machine':'Fx65',1:'register',2:'[i]'},
+		            {'args':2,'machine':'Ayyy',1:'i',2:'address'},
+		            {'args':2,'machine':'Fy15',1:'dt',2:'register'},
+		            {'args':2,'machine':'Fy18',1:'st',2:'register'},
+		            {'args':2,'machine':'Fy29',1:'f',2:'register'},
+		            {'args':2,'machine':'Fy33',1:'b',2:'register'},
+		            {'args':2,'machine':'Fy55',1:'[i]',2:'register'}],
+            'drw' :[{'args':3,'machine':'Dxyz',1:'register',2:'register',3:'nibble'}]}
 
 class blackbean:
 
@@ -61,7 +100,7 @@ class blackbean:
     def print_listing(self):
         # TODO print opcodes aswell
         for line in self.collection:
-            if line.get('asm'):
+            if line.get('address'):
                 print(format(line['address'], '#06x') + '    ' + line['original'], end="")
             else:
                 print((' ' * 10) + line['original'], end="")
@@ -70,11 +109,16 @@ class blackbean:
         print("TODO")
 
     def update_memory(self, tokens):
-        #TODO support db, dd.
+        if tokens.get('isEmpty'):
+            return
+        if tokens.get('tag'):
+            self.mmap[tokens.get('tag')] = self.address
         if tokens.get('asm'):
             tokens['address'] = self.address
-            self.mmap[tokens.get('tag')] = self.address
             self.address += 2
+        elif tokens.get('dataType'):
+            tokens['address'] = self.address
+            self.address += (len(tokens['declarations']) * tokens['dataType'])
 
     def tokenize(self,line):
         tokens = dict()
@@ -131,12 +175,16 @@ class blackbean:
             ddargs = ''.join(line_array).split(',')
             dec = []
             for arg in ddargs:
-                if arg[0] is '#':
+                if arg[0] is HEX_ESC:
+                    if len(arg) != (2 * tokens['dataType'])
+                        #TODO raise error
+                        print("ERROR: Data size of declare is incorrect.")
                     #TODO wrap int parse in try
                     val = int(arg[1:],16)
                 else:
+                    #TODO wrap int parse in try
                     val = int(arg)
-                if val > pow(256,tokens['dataType']):
+                if val >= pow(256,tokens['dataType']):
                     #TODO raise error
                     print("ERROR: Data declaration overflow.")
                 dec.append(val)
@@ -144,13 +192,13 @@ class blackbean:
             return tokens
 
         # Check for assembly instruction
-        if line_array[0].lower in OP_CODES:
+        if line_array[0].lower() in OP_CODES:
             #TODO continue to tokenize
             tokens['asm'] = " ".join(line_array)
             return tokens
 
         # Trash
-        print("ERROR: Unkown command")
+        print("ERROR: Unkown command ")
         return tokens
 
 def util_strip_comments(file_path, outpout_handler=None):
