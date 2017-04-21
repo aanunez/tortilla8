2af743a1cf6e6a6f2324cd145c70da5d510bb369
adam nunez <adam.a.nunez@gmail.com>
2017-04-20 19:32:03 -0500

    adding several insructions, guac not tested

--- guacamole.py
+++ guacamole.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
+import re
 import os
+import time
+import random
 import argparse
 from tortilla8_constants import *
 
@@ -8,65 +11,203 @@ from tortilla8_constants import *
 
 class guacamole:
 
-    def __init__(self):
+    def __init__(self, rom=None):
         self.ram = [None]*BYTES_OF_RAM
-        self.stack = []
         self.program_counter = PROGRAM_BEGIN_ADDRESS
+
+        # Stack emulation, only used if STACK_ADDRESS is not defined
+        self.stack = []
         self.stack_pointer = 0
 
         self.key_state = 0x00
         self.opcode = []
 
-        self.register = []
+        self.register = [0]*16
         self.index_register = 0
         self.delay_timer_register = 0
         self.sound_timer_register = 0
+
+        random.seed()
 
         # Load Font data
         for i,val in enumerate(FONT):
             self.ram[FONT_ADDRESS + i] = val
 
-        # Clear gfx buffer
+        self.clear_gfx()
+
+        # Load Rom
+        if rom is not None:
+            self.load_rom(self, rom)
+
+    def clear_gfx(self):
         for i in range(GFX_RESOLUTION):
             self.ram[GFX_ADDRESS + i] = 0x00
 
-    def load_rom(self, file_path):    # TODO should this take a handler or path?
+    def load_rom(self, file_path):
         file_size = os.path.getsize(file_path)
         if file_size > MAX_ROM_SIZE:
             # TODO raise error
             print("ERROR: Rom file too large")
-            return False
 
         with open(file_path, "rb") as fh:
             for i in range(file_size):
                 self.ram[PROGRAM_BEGIN_ADDRESS + i] = int.from_bytes(fh.read(1), 'big')
-        return True
 
     def reset(self):
         self.__init__()
 
-    def cpu_tick(self, instruction):
+    def run(self):
+        exit = False
+        audio_time = 0
+        cpu_time = 0
+        while not exit:
+            if CPU_WAIT_TIME <= (time.time() - cpu_time):
+                cpu_time = time.time()
+                self.cpu_tick()
+            if AUDIO_WAIT_TIME <= (time.time() - audio_time):
+                audio_time = time.time()
+                self.audio_tick()
 
-    def stack_push(self, val)
+    def cpu_tick(self):
+        # Fetch instruction from ram
+        instruction = hex(self.ram[self.program_counter])[2:] + hex(self.ram[self.program_counter + 1])[2:]
+        
+        for reg_pattern in OP_REG:
+            if re.match(reg_pattern, instruction):
+                execute_op_code(OP_REG[reg_pattern][0],OP_REG[reg_pattern][1],instruction)
+
+        self.program_counter += 2
+
+        # Draw to screen
+        # Update keys
+
+    def execute_op_code(self, mnemonic, version, hex_code):
+        #alias common stuff
+        reg1       = hex_code[1]
+        reg1_val   = self.register[int(hex_code[1])]
+        reg2       = hex_code[2]
+        reg2_val   = self.register[int(hex_code[1])]
+        lower_byte = hex_code[2:4]
+        addr       = int(hex_code[0:3], 16)
+
+        if mnemonic is 'cls':
+            self.clear_gfx()
+        
+        elif mnemoinc is 'ret':
+            self.program_counter = self.stack_pop()
+
+        elif mnemoinc is 'sys':
+            self.stack_push(self.program_counter)
+            self.program_counter = addr
+            
+        elif mnemoinc is 'call':
+            print("ERROR: RCA 1802 calls are not permitted")
+
+        elif mnemoinc is 'se':
+            ins_skip(1, reg1_val, version, 1 if "byte" in OP_CODES[mnemoinc][version] else 0 )
+
+        elif mnemoinc is 'sne':
+            ins_skip(0, reg1_val, version, 1 if "byte" in OP_CODES[mnemoinc][version] else 0 )
+
+        elif mnemoinc is 'shl':
+            if reg1_val >= 0x80:
+                self.register[reg1] = (reg1_val << 1) & 0xFF
+                self.register[0xF]  = 0xF
+            else:
+                self.register[reg1] = reg1_val << 1
+                self.register[0xF]  = 0x0
+
+        elif mnemoinc is 'shr':
+            self.register[reg1] = reg1_val >> 1
+            if (reg1_val % 2) == 1:
+                self.register[0xF]  = 0xF
+            else:
+                self.register[0xF]  = 0x0
+        
+        elif mnemoinc is 'or':
+            self.register[reg1] = reg1_val | reg2_val
+
+        elif mnemoinc is 'and':
+            self.register[reg1] = reg1_val & reg2_val
+
+        elif mnemoinc is 'xor':
+            self.register[reg1] = reg1_val ^ reg2_val
+
+        elif mnemoinc is 'sub':
+            ins_sub(self, reg1_val, reg2_val, reg1)
+
+        elif mnemoinc is 'subn':
+            ins_sub(self, reg2_val, reg1_val, reg1)
+
+        elif mnemoinc is 'jp':
+            self.stack_push(self.program_counter)
+            self.program_counter = addr + self.register[0]
+
+        elif mnemoinc is 'rnd':
+            self.resgister[reg1] = random.randint(0, 255) & lower_byte
+            
+        elif mnemoinc is 'add':
+            if 'byte' in OP_CODES[mnemoinc][version]:
+                self.resgister[reg1] += lower_byte
+            elif 'i' in OP_CODES[mnemoinc][version]:
+                self.index_register += reg1_val
+            else:
+                self.resgister[reg1] += reg2_val
+                if reg1_val + reg2_val > 0xFF:
+                    self.resgister[reg1] &= 0xFF          
+
+        elif mnemoinc is 'ld':
+
+        elif mnemoinc is 'drw'
+            height = int(hex_code[4],16)
+            origin = GFX_ADDRESS + reg1 + reg2
+            flag = False
+            for x in range(SPRITE_WIDTH):
+                for y in range(height):
+                    original = self.ram[origin + x + (y * GFX_WIDTH)]
+                    self.ram[origin + x + (y * GFX_WIDTH)] ^= self.ram[self.index_register + x + (y * GFX_WIDTH)]
+                    if not flag and ((self.ram[origin + x + (y * GFX_WIDTH)] ^ original) & original):
+                        flag = True
+            self.registers[0xF] = 0xFF
+
+    def ins_sub(self, reg1_val, reg2_val, store_reg):
+        if reg1_val > reg2_val:
+            self.register[store_reg] = reg1_val - reg2_val
+            self.register[0xF]  = 0xF
+        else:
+            self.register[store_reg] = 0xFF + reg1_val - reg2_val
+            self.register[0xF]  = 0x0
+
+    def ins_skip(self, equal, reg, is_byte, arg):
+        if ( reg == int(arg[:is_byte + 1],16) ) == equal:
+            self.program_counter += 2
+
+    def stack_push(self, val):
         if STACK_ADDRESS:
             self.ram[stack_pointer] = val
             stack_pointer += 1
+            if stack_pointer > STACK_SIZE:
+                print("ERROR: Stack overflow") 
         else:
             self.stack.append(val)
+            if len(self.stack) > STACK_SIZE:
+                print("ERROR: Stack overflow") 
 
-    def stack_pop(self, val)
+    def stack_pop(self):
         if STACK_ADDRESS:
-            stack_pointer += 1
+            stack_pointer -= 1
+            if stack_pointer < 0:
+                print("ERROR: Stack underflow")
             return self.ram[self.stack_pointer]
         else:
             return self.stack.pop()
 
-    def sound_tick(self)
+    def audio_tick(self):
         # Decrement sound registers
         if self.delay_timer_register != 0:
             self.delay_timer_register -= 1
-        if self.sound_timer_register != 0
-            self.delay_timer_register -= 1
+        if self.sound_timer_register != 0:
+            self.sound_timer_register -= 1
 
     def draw(self):
         print("draw")
@@ -77,7 +218,7 @@ class guacamole:
     def dump_ram(self):
         for i,val in enumerate(self.ram):
             if val is None:
-                 print('0x' + hex(i)[2:].zfill(3))
+                print('0x' + hex(i)[2:].zfill(3))
             else:
                 print('0x' + hex(i)[2:].zfill(3) + "  " + '0x' + hex(val)[2:].zfill(2))
 
--- tortilla8_constants.py
+++ tortilla8_constants.py
@@ -27,74 +27,118 @@ PROGRAM_BEGIN_ADDRESS=0x200
 OVERFLOW_ADDRESS=PROGRAM_BEGIN_ADDRESS+MAX_ROM_SIZE
 #PROGRAM_BEGIN_ADDRESS=0x600 #I dunno who uses this
 BYTES_OF_RAM=4096
-STACK_DEPTH=12
+STACK_SIZE=12
 #STACK_ADDRESS=0xEA0 #Not really used, original calls for
-GFX_RESOLUTION=int((64*32)/8)
+SPRITE_WIDTH
+GFX_HEIGHT=32
+GFX_WIDTH=64
+GFX_RESOLUTION=int((GFX_WIDTH*GFX_HEIGHT)/8)
 #GFX_RESOLUTION=int((64*48)/8) #Used by ETI 660
 #GFX_RESOLUTION=int((64*64)/8) #Used by ETI 660
 FONT_ADDRESS=0x050
 GFX_ADDRESS=0xF00
 
 # Data Type Enum - Probably not a good idea
-from enum import Enum
-class argument(Enum):
-    address  = auto()
-    register = auto()
-    byte     = auto()
-    nibble   = auto()
-    i        = auto()
-    [i]      = auto()
-    v0       = auto()
-    dt       = auto()
-    st       = auto()
-    b        = auto()
-    f        = auto()
+#from enum import Enum
+#class argument(Enum):
+#    address  = auto()
+#    register = auto()
+#    byte     = auto()
+#    nibble   = auto()
+#    i        = auto()
+#    [i]      = auto()
+#    v0       = auto()
+#    dt       = auto()
+#    st       = auto()
+#    b        = auto()
+#    f        = auto()
+#
+#    def __str__(self):
+#        return self.name
 
-    def __str__(self):
-        return self.name
+# Frequency
+CPU_HZ=60
+AUDIO_HZ=60
+CPU_WAIT_TIME=1/(CPU_HZ)
+AUDIO_WAIT_TIME=1/(AUDIO_HZ)
 
 # OP CODE related
 DATA_DECLARE={'db':1,'dw':2,'dd':4}
 OP_ARGS=0
 OP_HEX=1
 OP_CODE_SIZE=2
-OP_CODES={  'cls' :[[[],'00E0']],
-            'ret' :[[[],'00EE']],
-            'sys' :[[['address'],'1xxx']],
-            'call':[[['address'],'2xxx']],
-            'skp' :[[['register'],'Ex9E']],
-            'sknp':[[['register'],'ExA1']],
-            'se'  :[[['register','register'],'5xy0'],
-		            [['register','byte'],'3xyy']],
-            'sne' :[[['register','register'],'9xy0'],
-		            [['register','byte'],'4xyy']],
-            'add' :[[['register','byte'],'7xyy'],
-		            [['register','register'],'8xy4'],
-		            [['i','register'],'Fx1E']],
-            'or'  :[[['register','register'],'8xy1']],
-            'and' :[[['register','register'],'8xy2']],
-            'xor' :[[['register','register'],'8xy3']],    #TODO not in orig spec.
-            'sub' :[[['register','register'],'8xy5']],
-            'subn':[[['register','register'],'8xy7']],    #TODO not in orig spec.
-            'shr' :[[['register'],'8x06'],                #TODO not in orig spec.
-                    [['register','register'],'8xy6']],    #TODO not in orig spec.
-            'shl' :[[['register'],'8x0E'],                #TODO not in orig spec.
-                    [['register','register'],'8xyE']],    #TODO not in orig spec.
-            'rnd' :[[['register','byte'],'cxyy']],
-            'jp'  :[[['address'],'1xxx'],
-		            [['v0','address'],'Byyy']],           #This works! V0 is checked before generic regs
-            'ld'  :[[['register','byte'],'6xyy'],
-		            [['register','register'],'8xy0'],
-		            [['register','dt'],'Fx07'],
-		            [['register','k'],'Fx0A'],
-		            [['register','[i]'],'Fx65'],
-		            [['i','address'],'Ayyy'],
-		            [['dt','register'],'Fy15'],
-		            [['st','register'],'Fy18'],
-		            [['f','register'],'Fy29'],
-		            [['b','register'],'Fy33'],
-		            [['[i]','register'],'Fy55']],
-            'drw' :[[['register','register','nibble'],'Dxyz']]}
+OP_CODES={'cls' :[[[],'00E0']],
+          'ret' :[[[],'00EE']],
+          'sys' :[[['address'],'1xxx']],                #Jump with no offset
+          'call':[[['address'],'2xxx']],
+          'skp' :[[['register'],'Ex9E']],
+          'sknp':[[['register'],'ExA1']],
+          'se'  :[[['register','register'],'5xy0'],
+		          [['register','byte'],'3xyy']],
+          'sne' :[[['register','register'],'9xy0'],
+		          [['register','byte'],'4xyy']],
+          'add' :[[['register','byte'],'7xyy'],
+		          [['register','register'],'8xy4'],
+		          [['i','register'],'Fx1E']],
+          'or'  :[[['register','register'],'8xy1']],
+          'and' :[[['register','register'],'8xy2']],
+          'xor' :[[['register','register'],'8xy3']],    #TODO not in orig spec.
+          'sub' :[[['register','register'],'8xy5']],
+          'subn':[[['register','register'],'8xy7']],    #TODO not in orig spec.
+          'shr' :[[['register'],'8x06'],                #TODO not in orig spec.
+                  [['register','register'],'8xy6']],    #TODO not in orig spec.
+          'shl' :[[['register'],'8x0E'],                #TODO not in orig spec.
+                  [['register','register'],'8xyE']],    #TODO not in orig spec.
+          'rnd' :[[['register','byte'],'cxyy']],
+          'jp'  :[[['v0','address'],'Byyy'],
+                  [['address'],'1xxx']],                # jp == sys
+          'ld'  :[[['register','byte'],'6xyy'],
+		          [['register','register'],'8xy0'],
+		          [['register','dt'],'Fx07'],
+		          [['register','k'],'Fx0A'],
+		          [['register','[i]'],'Fx65'],
+		          [['i','address'],'Ayyy'],
+		          [['dt','register'],'Fy15'],
+		          [['st','register'],'Fy18'],
+		          [['f','register'],'Fy29'],
+		          [['b','register'],'Fy33'],
+		          [['[i]','register'],'Fy55']],
+          'drw' :[[['register','register','nibble'],'Dxyz']]}
+
+REG_OP={'00E0':['cls',0],
+        '00EE':['ret',0],
+        '1...':['sys',0],
+        '2...':['call',0],
+		'3...':['se',1],
+		'4...':['sne',1],
+        '5..0':['se',0],
+        '6...':['ld',0],
+        '7...':['add',0],
+		'8..0':['ld',1],
+        '8..1':['or',0],
+        '8..2':['and',0],
+        '8..3':['xor',0],
+		'8..4':['add',1],
+        '8..5':['sub',0],
+        '8..6':['shr',0], #TODO no support for emulating the "more correct" form of shift
+        '8..7':['subn',0],
+        '8..E':['shl',0], #TODO above 
+        '9..0':['sne',0],
+		'A...':['ld',5],
+		'B...':['jp',0],
+        'C...':['rnd',0], 
+        'D...':['drw',0],
+        'E.9E':['skp',0],
+        'E.A1':['sknp',0],
+		'F.07':['ld',2],
+		'F.0A':['ld',3],
+		'F.15':['ld',6],
+		'F.18':['ld',7],
+		'F.1E':['add',2],
+		'F.29':['ld',8],
+		'F.33':['ld',9],
+		'F.55':['ld',10],
+		'F.65':['ld',4]}
 
 # Fonts (80 bytes)
 FONT=[
