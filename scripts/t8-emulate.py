857fc2fa413a2bdb5f451f303c88d16d58871ce3
adam nunez <adam.a.nunez@gmail.com>
2017-05-17 20:19:17 -0500

    Script cleanup for emualtor and gui

--- scripts/t8-emulate.py
+++ scripts/t8-emulate.py
@@ -4,34 +4,33 @@ import os
 import argparse
 from tortilla8.platter import platter
 
-def parse_args():
-    parser = argparse.ArgumentParser(description='Platter is a text based front end for the Chip-8 emulator guacamole. ')
-    parser.add_argument('rom', help='ROM to load and play.')
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument('-f','--frequency', default=10,help='CPU frequency (in Hz) to target, minimum 1 hz.')
-    group.add_argument('-s','--step', help='Start the emulator is "step" mode.',action='store_true')
-    parser.add_argument('-d','--drawfix', help='Enable anti-flicker, stops platter from drawing to screen when sprites are only removed.', action='store_true')
-    parser.add_argument('-i','--initram', help='Initialize RAM to all zero values. Needed to run some ROMs that assume untouched addresses to be zero.', action='store_true')
-    parser.add_argument('-a','--audio', help='Path to audio to play for Sound Timer, or "off" to prevent sound from playing.')
-    opts = parser.parse_args()
+def pos_int(value):
+    ivalue = int(value)
+    if ivalue < 1:
+         raise argparse.ArgumentTypeError("%s is an invalid positive int value." % value)
+    return ivalue
 
-    if not os.path.isfile(opts.rom):
-        raise OSError("File '" + opts.rom + "' does not exist")
+parser = argparse.ArgumentParser(description=
+'''
+Start a text (unicdoe) based Chip8 emulator.
+''')
+parser.add_argument('rom', help='ROM to load and play.')
+group = parser.add_mutually_exclusive_group()
+group.add_argument('-f','--frequency', type=pos_int, default=10,help='CPU frequency (in Hz) to target, minimum 1 hz.')
+group.add_argument('-s','--step',     help='Start the emulator is "step" mode.',action='store_true')
+parser.add_argument('-d','--drawfix', help='Enable anti-flicker, stops platter from drawing to screen when sprites are only removed.', action='store_true')
+parser.add_argument('-i','--initram', help='Initialize RAM to all zero values. Needed to run some ROMs that assume untouched addresses to be zero.', action='store_true')
+parser.add_argument('-a','--audio',   help='Path to audio to play for Sound Timer, or "off" to prevent sound from playing.')
+parser.add_argument("-st","--soundtimer", type=pos_int, default=60, help='Frequency (in Hz) to target for the audio timmer.')
+parser.add_argument("-dt","--delaytimer", type=pos_int, default=60, help='Frequency (in Hz) to target for the delay timmer.')
+opts = parser.parse_args()
 
-    if opts.frequency:
-        try: opts.frequency = int(opts.frequency)
-        except: raise ValueError("Non-numeric frequency provided")
-        if opts.frequency < 1:
-            raise ValueError("Please use step mode for sub 1 hz operation")
+if not os.path.isfile(opts.rom):
+    raise OSError("File '" + opts.rom + "' does not exist")
 
-    if opts.step:
-        opts.frequency = 1000000 # 1 Ghz
+if opts.step:
+    opts.frequency = 1000000 # 1 Ghz
 
-    return opts
-
-def main(opts):
-    disp = platter(opts.rom, opts.frequency, 60, 60, opts.initram, opts.drawfix, opts.audio)
-    disp.start(opts.step)
-
-main(parse_args())
+disp = platter(opts.rom, opts.frequency, opts.soundtimer, opts.delaytimer, opts.initram, opts.drawfix, opts.audio)
+disp.start(opts.step)
 
--- scripts/t8-execute.py
+++ scripts/t8-execute.py
@@ -4,30 +4,32 @@ import os
 import argparse
 from tortilla8.guacamole import guacamole
 
-def parse_args():
-    parser = argparse.ArgumentParser(description='Guacamole is a Chip-8 emulator ...')
-    parser.add_argument('rom', help='ROM to load and play.')
-    parser.add_argument("-f","--frequency", default=5,help='Frequency (in Hz) to target for CPU.')
-    parser.add_argument("-st","--soundtimer", default=60,help='Frequency (in Hz) to target for the audio timmer.')
-    parser.add_argument("-dt","--delaytimer", default=60,help='Frequency (in Hz) to target for the delay timmer.')
-    parser.add_argument('-i','--initram', help='Initialize RAM to all zero values.', action='store_true')
-    opts = parser.parse_args()
+def pos_int(value):
+    ivalue = int(value)
+    if ivalue < 1:
+         raise argparse.ArgumentTypeError("%s is an invalid positive int value." % value)
+    return ivalue
 
-    if not os.path.isfile(opts.rom):
-        raise OSError("File '" + opts.rom + "' does not exist.")
+parser = argparse.ArgumentParser(description=
+'''
+Execute a rom to quickly check for errors, instructions are printed to screen
+immediately after execution of that operation code.
+''')
+parser.add_argument('rom', help='ROM to load and play.')
+parser.add_argument("-f","--frequency",   type=pos_int, default=5,  help='Frequency (in Hz) to target for CPU.')
+parser.add_argument("-st","--soundtimer", type=pos_int, default=60, help='Frequency (in Hz) to target for the audio timmer.')
+parser.add_argument("-dt","--delaytimer", type=pos_int, default=60, help='Frequency (in Hz) to target for the delay timmer.')
+parser.add_argument('-i','--initram', help='Initialize RAM to all zero values.', action='store_true')
+opts = parser.parse_args()
 
-    if opts.frequency:
-        opts.frequency = int(opts.frequency)
+if not os.path.isfile(opts.rom):
+    raise OSError("File '" + opts.rom + "' does not exist.")
 
-    return opts
+guac = guacamole(opts.rom, opts.frequency, opts.soundtimer, opts.delaytimer, opts.initram)
+guac.log_to_screen = True
+try:
+    while True:
+        guac.run()
+except KeyboardInterrupt:
+    pass
 
-def main(opts):
-    guac = guacamole(opts.rom, opts.frequency, opts.soundtimer, opts.delaytimer, opts.initram)
-    guac.log_to_screen = True
-    try:
-        while True:
-            guac.run()
-    except KeyboardInterrupt:
-        pass
-
-main(parse_args())
