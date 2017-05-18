e80a4f2a281aefe8c59e0db6afadad7a7f05226a
adam nunez <adam.a.nunez@gmail.com>
2017-05-18 03:52:23 -0500

    Corrections to setup and script names.

--- setup.py
+++ setup.py
@@ -3,23 +3,24 @@
 from setuptools import setup
 
 setup(
-	name='tortilla8',
-	version='0.1',
-	description='A collection of Chip8 tools for per-processing, assembling, emulating, disassembling, and visualizing Chip8 ROMs.',
-	author='Adam Nunez',
-	author_email='adam.a.nunez@gmail.com',
-	url='https://github.com/aanunez/tortilla8',
-	packages=['tortilla8'], 
-	install_requires=[], #Curses and SimpleAudio are optional
-    extras_require={
+	name = 'tortilla8',
+	version = '0.1',
+	description = 'A collection of Chip8 tools for pre-processing, assembling, emulating, disassembling, and visualizing Chip8 ROMs.',
+	author = 'Adam Nunez',
+	author_email = 'adam.a.nunez@gmail.com',
+	license = 'GPLv3',
+	url = 'https://github.com/aanunez/tortilla8',
+	packages = ['tortilla8'],
+	#install_requires = [], #Curses and SimpleAudio are optional
+    extras_require = {
         'Emulation Sound':  ["simpleaudio"],
-    }
-	scripts=[
+    },
+	scripts = [
 		    'scripts/t8-assemble',
 		    'scripts/t8-disassemble',
 		    'scripts/t8-execute',
 		    'scripts/t8-preproc',
 		    'scripts/t8-emulate'
-		   ]
-	include_package_data=True
+		   ],
+	include_package_data = True
 )
