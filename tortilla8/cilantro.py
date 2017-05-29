b284614e2f98ad0072b72aa9a9ee77d2b64cf9c9
adam nunez <adam.a.nunez@gmail.com>
2017-05-28 20:42:48 -0500

    Moving cilantro and salsa to functions, no longer classes

--- tortilla8/cilantro.py
+++ tortilla8/cilantro.py
@@ -1,97 +1,93 @@
 #!/usr/bin/python3
 
+from collections import namedtuple
 from .constants.preprocessor import END_MEM_TAG, PRE_PROC, DATA_DECLARE
 from .constants.symbols import BEGIN_COMMENT
 from .constants.opcodes import OP_CODES
 
-#TODO Remove class, should just be a function that returns a tuple/dict
-
-class cilantro:
+def cilantro(line, line_number):
     '''
-    Lexer/tokenizer for Chip 8 instructions. Used by Blackbean (assembler)
-    and Jalapeno (pre-processor).
+    Cilantro is a lexer/tokenizer for Chip 8 instructions. Used by Blackbean
+    (assembler) and Jalapeno (pre-processor).
     '''
+    c = namedtuple('cilantro_data', 'is_empty original comment mem_tag \
+        mem_address line_numb pp_directive pp_args pp_line data_declarations \
+        data_size dd_ints instruction arguments instruction_int')
+    c.is_empty = True         #Default, is blank or comment only
+    c.original = line         #whole, unedited, line
+    c.comment  = ""           #comment on the line
+    c.mem_tag  = ""           #tag/label for the line, no ":"
+    c.mem_address  = 0        #address
+    c.line_numb = line_number #1 index line number
 
-    def __init__(self, line, line_number):
-        """
-        Intializes by parsing the line and storing the line_number.
-        The object is then used as a data container for Blackbean.
-        """
-        self.is_empty = True         #Default, is blank or comment only
-        self.original = line         #whole, unedited, line
-        self.comment  = ""           #comment on the line
-        self.mem_tag  = ""           #tag/label for the line, no ":"
-        self.mem_address  = 0        #address
-        self.line_numb = line_number #1 index line number
+    c.pp_directive = ""       #pre-procs directive on the line
+    c.pp_args      = []       #arguments after or surounding a pp directive
+    c.pp_line      = ""       #line with pp applied
 
-        self.pp_directive = ""       #pre-procs directive on the line
-        self.pp_args      = []       #arguments after or surounding a pp directive
-        self.pp_line      = ""       #line with pp applied
+    c.data_declarations = []  #list of strings in original form
+    c.data_size         = 0   #size (in bytes) of data defined on line
+    c.dd_ints           = []  #ints for machine code of size dataType
 
-        self.data_declarations = []  #list of strings in original form
-        self.data_size         = 0   #size (in bytes) of data defined on line
-        self.dd_ints           = []  #ints for machine code of size dataType
+    c.instruction     = ""    #"cls", "ret" etc
+    c.arguments       = []    #list of args in original form
+    c.instruction_int = None  #single int for machine code
 
-        self.instruction     = ""    #"cls", "ret" etc
-        self.arguments       = []    #list of args in original form
-        self.instruction_int = None  #single int for machine code
+    line = line.lstrip()
 
-        line = line.lstrip()
+    # Remove Blanks
+    if line.isspace() or not line:
+        return c
 
-        # Remove Blanks
-        if line.isspace() or not line:
-            return
+    # Remove Comment only lines
+    if line.lstrip().startswith(BEGIN_COMMENT):
+        c.comment = line.rstrip()
+        return c
 
-        # Remove Comment only lines
-        if line.lstrip().startswith(BEGIN_COMMENT):
-            self.comment = line.rstrip()
-            return
+    c.is_empty = False
 
-        self.is_empty = False
+    # Check for any comments
+    c.comment = ''.join(line.split(BEGIN_COMMENT)[1:])
+    line = line.split(BEGIN_COMMENT)[0].rstrip()
 
-        # Check for any comments
-        self.comment = ''.join(line.split(BEGIN_COMMENT)[1:])
-        line = line.split(BEGIN_COMMENT)[0].rstrip()
+    # Breakout into array
+    line_array = line.lower().split()
 
-        # Breakout into array
-        line_array = line.lower().split()
+    # Check if tag exists, must be left most
+    if line_array[0].endswith(END_MEM_TAG):
+        c.mem_tag = line_array[0][:-1]
+        line_array.pop(0)
+        if not line_array:
+            return c
 
-        # Check if tag exists, must be left most
-        if line_array[0].endswith(END_MEM_TAG):
-            self.mem_tag = line_array[0][:-1]
-            line_array.pop(0)
-            if not line_array:
-                return
+    # If there are additional tags raise error
+    if END_MEM_TAG in ''.join(line_array):
+        raise RuntimeError("Multiple Memory Tags found on line " + str(c.line_numb))
 
-        # If there are additional tags raise error
-        if END_MEM_TAG in ''.join(line_array):
-            raise RuntimeError("Multiple Memory Tags found on line " + str(self.line_numb))
+    # Check for any pre-processor commands
+    for i,word in enumerate(line_array):
+        if word in PRE_PROC:
+            c.pp_directive = word
+            line_array.pop(i)
+            c.pp_args = line_array
+            return c
 
-        # Check for any pre-processor commands
-        for i,word in enumerate(line_array):
-            if word in PRE_PROC:
-                self.pp_directive = word
-                line_array.pop(i)
-                self.pp_args = line_array
-                return
+    # Check for data declarations
+    if line_array[0] in DATA_DECLARE:
+        c.data_size = DATA_DECLARE[line_array[0]]
+        line_array.pop(0)
+        if not line_array:
+            raise RuntimeError("Expected data declaration on line " + str(c.line_numb))
+        c.data_declarations = ''.join(line_array).split(',')
+        return c
 
-        # Check for data declarations
-        if line_array[0] in DATA_DECLARE:
-            self.data_size = DATA_DECLARE[line_array[0]]
-            line_array.pop(0)
-            if not line_array:
-                raise RuntimeError("Expected data declaration on line " + str(self.line_numb))
-            self.data_declarations = ''.join(line_array).split(',')
-            return
+    # Check for assembly instruction
+    if line_array[0] in OP_CODES:
+        c.instruction = line_array[0]
+        line_array.pop(0)
+        c.arguments = []
+        if line_array:
+            c.arguments = ''.join(line_array).split(',')
+        return c
 
-        # Check for assembly instruction
-        if line_array[0] in OP_CODES:
-            self.instruction = line_array[0]
-            line_array.pop(0)
-            self.arguments = []
-            if line_array:
-                self.arguments = ''.join(line_array).split(',')
-            return
-
-        # Trash
-        raise RuntimeError("Cannot parse line " + str(self.line_numb))
+    # Trash
+    raise RuntimeError("Cannot parse line " + str(c.line_numb))
--- tortilla8/salsa.py
+++ tortilla8/salsa.py
@@ -1,67 +1,66 @@
 #!/usr/bin/python3
 
 import re
+from collections import namedtuple
 from .constants.opcodes import OP_REG, OP_ARGS, OP_CODES, UNOFFICIAL_OP_CODES
 
-#TODO Remove class, should just be a function that returns a tuple/dict
-
-class salsa:
+def salsa(byte_list):
     '''
+    Salsa is a one line (2 byte) dissassembler function for CHIP-8 Roms. It
+    returns a named tuple with various information on the line.
     '''
+    s = namedtuple('salsa_data', 'hex_instruction is_valid mnemonic\
+        mnemonic_arg_types disassembled_line unoffical_op')
+    s.hex_instruction =  hex( byte_list[0] )[2:].zfill(2)
+    s.hex_instruction += hex( byte_list[1] )[2:].zfill(2)
+    s.is_valid = False
+    s.mnemonic = None
+    s.mnemonic_arg_types = None
+    s.disassembled_line = ""
+    s.unoffical_op = False
 
-    def __init__(self, byte_list):
-        '''
-        '''
+    # Match the instruction via a regex index
+    for mnemonic, reg_patterns in OP_CODES.items():
+        for pattern_version in reg_patterns:
+            if not re.match(pattern_version[OP_REG], s.hex_instruction): continue
+            s.mnemonic = mnemonic
+            s.mnemonic_arg_types = pattern_version[OP_ARGS]
+            s.is_valid = True
+            break
+        if s.is_valid:
+            break
 
-        self.hex_instruction =  hex( byte_list[0] )[2:].zfill(2)
-        self.hex_instruction += hex( byte_list[1] )[2:].zfill(2)
-        self.is_valid = False
-        self.mnemonic = None
-        self.mnemonic_arg_types = None
-        self.disassembled_line = ""
-        self.unoffical_op = False
+    # If not a valid instruction, assume data
+    if not s.is_valid:
+        s.disassembled_line = s.hex_instruction
+        return s
 
-        # Match the instruction via a regex index
-        for mnemonic, reg_patterns in OP_CODES.items():
-            for pattern_version in reg_patterns:
-                if not re.match(pattern_version[OP_REG], self.hex_instruction): continue
-                self.mnemonic = mnemonic
-                self.mnemonic_arg_types = pattern_version[OP_ARGS]
-                self.is_valid = True
-                break
-            if self.is_valid:
-                break
+    # If unoffical, flag it.
+    if s.mnemonic in UNOFFICIAL_OP_CODES:
+        s.unoffical_op = True
 
-        # If not a valid instruction, assume data
-        if not self.is_valid:
-            self.disassembled_line = self.hex_instruction
-            return
+    # No args to parse
+    if s.mnemonic_arg_types is None:
+        s.disassembled_line = s.mnemonic
+        return s
 
-        # If unoffical, flag it.
-        if self.mnemonic in UNOFFICIAL_OP_CODES:
-            self.unoffical_op = True
+    # Parse Args
+    tmp = ''
+    reg_numb = 1
+    for arg_type in s.mnemonic_arg_types:
+        if arg_type is 'register':
+            tmp = 'v'+s.hex_instruction[reg_numb]
+        elif arg_type is 'byte':
+            tmp = '#'+s.hex_instruction[2:]
+        elif arg_type is 'address':
+            tmp = '#'+s.hex_instruction[1:]
+        elif arg_type is 'nibble':
+            tmp = '#'+s.hex_instruction[3]
+        else:
+            tmp = arg_type
+        s.disassembled_line += tmp.ljust(5) + ','
+        reg_numb = 2
 
-        # No args to parse
-        if self.mnemonic_arg_types is None:
-            self.disassembled_line = self.mnemonic
-            return
-
-        # Parse Args
-        tmp = ''
-        reg_numb = 1
-        for arg_type in self.mnemonic_arg_types:
-            if arg_type is 'register':
-                tmp = 'v'+self.hex_instruction[reg_numb]
-            elif arg_type is 'byte':
-                tmp = '#'+self.hex_instruction[2:]
-            elif arg_type is 'address':
-                tmp = '#'+self.hex_instruction[1:]
-            elif arg_type is 'nibble':
-                tmp = '#'+self.hex_instruction[3]
-            else:
-                tmp = arg_type
-            self.disassembled_line += tmp.ljust(5) + ','
-            reg_numb = 2
-
-        self.disassembled_line = (self.mnemonic.ljust(5) + self.disassembled_line[:-1]).rstrip()
+    s.disassembled_line = (s.mnemonic.ljust(5) + s.disassembled_line[:-1]).rstrip()
+    return s
 
