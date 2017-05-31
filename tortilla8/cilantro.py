ad828a426090bebf05e743c0c05b193741e6ee8a
Adam Nunez <adam.nunez@wisc.edu>
2017-05-31 10:18:18 -0500

    Restoring Cilantro to class from previous commit

--- tortilla8/cilantro.py
+++ tortilla8/cilantro.py
@@ -1,109 +1,97 @@
 #!/usr/bin/python3
 
-from collections import namedtuple
 from .constants.preprocessor import END_MEM_TAG, PRE_PROC, DATA_DECLARE
 from .constants.symbols import BEGIN_COMMENT
 from .constants.opcodes import OP_CODES
 
-cilantro_data = namedtuple('cilantro_data', 'is_empty original \
-    comment mem_tag mem_address line_numb pp_directive pp_args \
-    pp_line data_declarations data_size dd_ints instruction \
-    arguments instruction_int')
+#TODO Remove class, should just be a function that returns a tuple/dict
 
-def cilantro(line, line_number):
+class cilantro:
     '''
-    Cilantro is a lexer/tokenizer for Chip 8 instructions. Used by Blackbean
-    (assembler) and Jalapeno (pre-processor).
+    Lexer/tokenizer for Chip 8 instructions. Used by Blackbean (assembler)
+    and Jalapeno (pre-processor).
     '''
-    is_empty = True,         #Default, is blank or comment only
-    original = line,         #whole, unedited, line
-    comment  = "",           #comment on the line
-    mem_tag  = "",           #tag/label for the line, no ":"
-    mem_address  = 0,        #address
-    line_numb = line_number, #1 index line number
 
-    pp_directive = "",       #pre-procs directive on the line
-    pp_args      = [],       #arguments after or surounding a pp directive
-    pp_line      = "",       #line with pp applied
+    def __init__(self, line, line_number):
+        """
+        Intializes by parsing the line and storing the line_number.
+        The object is then used as a data container for Blackbean.
+        """
+        self.is_empty = True         #Default, is blank or comment only
+        self.original = line         #whole, unedited, line
+        self.comment  = ""           #comment on the line
+        self.mem_tag  = ""           #tag/label for the line, no ":"
+        self.mem_address  = 0        #address
+        self.line_numb = line_number #1 index line number
 
-    data_declarations = [],  #list of strings in original form
-    data_size         = 0,   #size (in bytes) of data defined on line
-    dd_ints           = [],  #ints for machine code of size dataType
+        self.pp_directive = ""       #pre-procs directive on the line
+        self.pp_args      = []       #arguments after or surounding a pp directive
+        self.pp_line      = ""       #line with pp applied
 
-    instruction     = "",    #"cls", "ret" etc
-    arguments       = [],    #list of args in original form
-    instruction_int = None   #single int for machine code
+        self.data_declarations = []  #list of strings in original form
+        self.data_size         = 0   #size (in bytes) of data defined on line
+        self.dd_ints           = []  #ints for machine code of size dataType
 
-    line = line.lstrip()
+        self.instruction     = ""    #"cls", "ret" etc
+        self.arguments       = []    #list of args in original form
+        self.instruction_int = None  #single int for machine code
 
-    # Remove Blanks
-    if line.isspace() or not line:
-        return cilantro_data(is_empty, original, comment, mem_tag, mem_address,
-            line_numb, pp_directive, pp_args, pp_line, data_declarations,
-            data_size, dd_ints, instruction, arguments, instruction_int)
+        line = line.lstrip()
 
-    # Remove Comment only lines
-    if line.lstrip().startswith(BEGIN_COMMENT):
-        c.comment = line.rstrip()
-        return cilantro_data(is_empty, original, comment, mem_tag, mem_address,
-            line_numb, pp_directive, pp_args, pp_line, data_declarations,
-            data_size, dd_ints, instruction, arguments, instruction_int)
+        # Remove Blanks
+        if line.isspace() or not line:
+            return
 
-    c.is_empty = False
+        # Remove Comment only lines
+        if line.lstrip().startswith(BEGIN_COMMENT):
+            self.comment = line.rstrip()
+            return
 
-    # Check for any comments
-    c.comment = ''.join(line.split(BEGIN_COMMENT)[1:])
-    line = line.split(BEGIN_COMMENT)[0].rstrip()
+        self.is_empty = False
 
-    # Breakout into array
-    line_array = line.lower().split()
+        # Check for any comments
+        self.comment = ''.join(line.split(BEGIN_COMMENT)[1:])
+        line = line.split(BEGIN_COMMENT)[0].rstrip()
 
-    # Check if tag exists, must be left most
-    if line_array[0].endswith(END_MEM_TAG):
-        c.mem_tag = line_array[0][:-1]
-        line_array.pop(0)
-        if not line_array:
-            return cilantro_data(is_empty, original, comment, mem_tag, mem_address,
-                line_numb, pp_directive, pp_args, pp_line, data_declarations,
-                data_size, dd_ints, instruction, arguments, instruction_int)
+        # Breakout into array
+        line_array = line.lower().split()
 
-    # If there are additional tags raise error
-    if END_MEM_TAG in ''.join(line_array):
-        raise RuntimeError("Multiple Memory Tags found on line " + str(c.line_numb))
+        # Check if tag exists, must be left most
+        if line_array[0].endswith(END_MEM_TAG):
+            self.mem_tag = line_array[0][:-1]
+            line_array.pop(0)
+            if not line_array:
+                return
 
-    # Check for any pre-processor commands
-    for i,word in enumerate(line_array):
-        if word in PRE_PROC:
-            c.pp_directive = word
-            line_array.pop(i)
-            c.pp_args = line_array
-            return cilantro_data(is_empty, original, comment, mem_tag, mem_address,
-                line_numb, pp_directive, pp_args, pp_line, data_declarations,
-                data_size, dd_ints, instruction, arguments, instruction_int)
+        # If there are additional tags raise error
+        if END_MEM_TAG in ''.join(line_array):
+            raise RuntimeError("Multiple Memory Tags found on line " + str(self.line_numb))
 
-    # Check for data declarations
-    if line_array[0] in DATA_DECLARE:
-        c.data_size = DATA_DECLARE[line_array[0]]
-        line_array.pop(0)
-        if not line_array:
-            raise RuntimeError("Expected data declaration on line " + str(c.line_numb))
-        c.data_declarations = ''.join(line_array).split(',')
-        return cilantro_data(is_empty, original, comment, mem_tag, mem_address,
-            line_numb, pp_directive, pp_args, pp_line, data_declarations,
-            data_size, dd_ints, instruction, arguments, instruction_int)
+        # Check for any pre-processor commands
+        for i,word in enumerate(line_array):
+            if word in PRE_PROC:
+                self.pp_directive = word
+                line_array.pop(i)
+                self.pp_args = line_array
+                return
 
-    # Check for assembly instruction
-    if line_array[0] in OP_CODES:
-        c.instruction = line_array[0]
-        line_array.pop(0)
-        c.arguments = []
-        if line_array:
-            c.arguments = ''.join(line_array).split(',')
-        return cilantro_data(is_empty, original, comment, mem_tag, mem_address,
-            line_numb, pp_directive, pp_args, pp_line, data_declarations,
-            data_size, dd_ints, instruction, arguments, instruction_int)
+        # Check for data declarations
+        if line_array[0] in DATA_DECLARE:
+            self.data_size = DATA_DECLARE[line_array[0]]
+            line_array.pop(0)
+            if not line_array:
+                raise RuntimeError("Expected data declaration on line " + str(self.line_numb))
+            self.data_declarations = ''.join(line_array).split(',')
+            return
 
-    # Trash
-    raise RuntimeError("Cannot parse line " + str(c.line_numb))
+        # Check for assembly instruction
+        if line_array[0] in OP_CODES:
+            self.instruction = line_array[0]
+            line_array.pop(0)
+            self.arguments = []
+            if line_array:
+                self.arguments = ''.join(line_array).split(',')
+            return
 
-
+        # Trash
+        raise RuntimeError("Cannot parse line " + str(self.line_numb))
