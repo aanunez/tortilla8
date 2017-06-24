bcafcd108fead982b67fe25decf8b85cb0f97aae
adam nunez <adam.a.nunez@gmail.com>
2017-06-24 16:35:59 -0500

    new method of building __all__ added

--- tortilla8/__init__.py
+++ tortilla8/__init__.py
@@ -1,13 +1,44 @@
 #!/usr/bin/env python3
 
-from os.path import dirname, basename, isfile
-from glob import glob
-modules = glob(dirname(__file__)+"/*.py")
-__all__ = [ basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py') ]
+# This snipit adds EVERYTHING to __all__
+#from os.path import dirname, basename, isfile
+#from glob import glob
+#modules = glob(dirname(__file__)+"/*.py")
+#__all__ = [ basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py') ]
 
-from . import blackbean
-from . import cilantro
-from . import guacamole
-from . import jalapeno
-from . import platter
-from . import salsa
+__all__ = []
+
+def export(defn):
+    globals()[defn.__name__] = defn
+    __all__.append(defn.__name__)
+    return defn
+
+from enum import Enum
+@export
+class Emulation_Error(Enum):
+    _Information = 1
+    _Warning     = 2
+    _Fatal       = 3
+
+    def __str__(self):
+        return self.name[1:]
+
+    @classmethod
+    def from_string(self, value):
+        value = value.lower()
+        if (value == "info") or (value == "information"):
+            return self._Information
+        if (value == "warning"):
+            return self._Warning
+        if (value == "fatal"):
+            return self._Fatal
+        return None
+
+# Skipping platter and instructions, they are not useful to programmers
+from .blackbean import *
+from .cilantro import *
+from .guacamole import *
+from .jalapeno import *
+from .salsa import *
+
+
